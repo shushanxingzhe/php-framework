ff5712967188de8cba9ba8151809f14f6344fba9
shushanxingzhe <findwayinsea@gmail.com>
2017-03-10 17:41:30 +0800

    first commit

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/** Configuration Variables **/
+
+define ('DEVELOPMENT_ENVIRONMENT',true);
+
+
+define('DB_NAME', 'framework');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '123456');
+define('DB_HOST', '192.168.117.128');
+
+define('BASE_PATH','http://framework.devbase.com');
+
+
+define('PAGINATE_LIMIT', '5');
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(dirname(__FILE__)));
+
+
+require_once (ROOT . DS . 'library' . DS . 'bootstrap.php');
